93686bb73b1b8fb532f1649cc3ddf2cb4d4bfba9
daikitag <48062118+daikitag@users.noreply.github.com>
2023-04-07 19:45:06 +0900

    Unit test update
    Updated unit test

--- tests/test_phenotype.py
+++ tests/test_phenotype.py
@@ -17,105 +17,215 @@ class Test_update_node_values_array_access:
         node_values = np.array([1,1,1,1,1,1,1,0,1,0,1,1,1])
         
         # Create an arbitrary genotype vector
-        G = np.random.normal(loc=0, scale=1, size=6)        
+        rng = np.random.default_rng(1)
+        G = rng.normal(loc=0, scale=1, size=6)        
         G1 = np.copy(G)
         G = sim_pheno.update_node_values_array_access(root, left_child_array, right_sib_array, node_values, G)
         
         actual_G = np.array([4,4,4,4,3,3]) + G1
         
         assert np.array_equal(G, actual_G)
+      
+    def binary_tree(self):
+        # 3.00   6     
+        #     ┊ ┏━┻━┓    ┊
+        # 2.00┊ ┃   5   ┊
+        #     ┊ ┃ ┏━┻┓   ┊
+        # 1.00┊ ┃ ┃  4  ┊
+        #     ┊ ┃ ┃ ┏┻┓  ┊
+        # 0.00 0 1 2 3 
+        #              
+        # Mutation in all edges
+        ts = tskit.Tree.generate_comb(4, span=10).tree_sequence
+        tables = ts.dump_tables()
+        for j in range(6):
+            tables.sites.add_row(j, "A")
+            tables.mutations.add_row(site=j, node=j, derived_state="T")
+        ts = tables.tree_sequence()
+        tree = ts.first()
+        node_values = np.zeros(ts.num_nodes)
+        for mut in tree.mutations():
+            node_values[mut.node] += 1
         
+        rng = np.random.default_rng(2)
+        G = rng.normal(loc=0, scale=1, size=ts.num_samples)
+        G1 = np.copy(G)
+        G = sim_pheno.update_node_values_array_access(tree.root, tree.left_child_array, tree.right_sib_array, node_values, G)
+        
+        actual_G = np.array([1,2,3,3]) + G1
+        
+        assert np.array_equal(G, actual_G)
+    
+    def non_binary_tree(self):
+
+        # 2.00      7   
+        #     ┊ ┏━┏━━┏━┻━━━┓   ┊
+        # 1.00┊ ┃ ┃ ┃    6   ┊
+        #     ┊ ┃ ┃ ┃  ┏━┳┻┳━┓ ┊
+        # 0.00 0 1 2  3 4 5
+        #              
+        # Mutation in all edges
+        ts = tskit.Tree.generate_balanced(6, arity=4, span=10).tree_sequence
+        tables = ts.dump_tables()
+        for j in range(7):
+            tables.sites.add_row(j, "A")
+            tables.mutations.add_row(site=j, node=j, derived_state="T")
+        ts = tables.tree_sequence()
+        tree = ts.first()
+        node_values = np.zeros(ts.num_nodes)
+        for mut in tree.mutations():
+            node_values[mut.node] += 1
+        
+        rng = np.random.default_rng(3)
+        G = rng.normal(loc=0, scale=1, size=ts.num_samples)
+        G1 = np.copy(G)
+        G = sim_pheno.update_node_values_array_access(tree.root, tree.left_child_array, tree.right_sib_array, node_values, G)
+        
+        actual_G = np.array([1,1,1,2,2,2]) + G1
+        
+        assert np.array_equal(G, actual_G)
 
 class Test_environment:
-    def test_environment(self):
-        size_list = np.array([1, 10, 100, 1000, 10000])
-        h2_list = np.array([0.1, 0.3, 0.5, 0.7, 0.9])
-        for size in size_list:
-            for h2 in h2_list:
-                G = np.random.normal(size = size)
-                phenotype, E = sim_pheno.environment(G, h2)
-                assert len(phenotype) == size
-                assert len(E) == size
-    def test_error(self):
-        size_list = np.array([1, 10, 100, 1000, 10000])
-        h2_list = np.array([-0.2, 0, 1, 1.2])
-        for h2 in h2_list:
-            for size in size_list:
-                G = np.random.normal(size = size)            
-                with pytest.raises(ValueError):
-                    phenotype, E = sim_pheno.environment(G, h2)    
+    @pytest.mark.parametrize("size", [1, 2, 10, 100])
+    @pytest.mark.parametrize("h2", [0, 0.1, 0.3, 0.5, 0.7, 0.9, 1])
+    def test_environment(self, size, h2):
+        rng = np.random.default_rng(1)
+        G = rng.normal(size = size)
+        phenotype, E = sim_pheno.environment(G, h2, 1)
+        assert len(phenotype) == size
+        assert len(E) == size
+        assert np.issubdtype(phenotype.dtype, np.floating)
+        assert np.issubdtype(E.dtype, np.floating)
+    
+    @pytest.mark.parametrize("h2", [0, 0.1, 0.3, 0.5, 0.7, 0.9, 1])
+    def test_environment_G_error(self, h2):
+        rng = np.random.default_rng(1)
+        G = rng.normal(size = 0)
+        with pytest.raises(ValueError, match="No individuals in the simulation model"):
+            phenotype, E = sim_pheno.environment(G, h2, 1)
+    
+    @pytest.mark.parametrize("size", [1, 2, 10, 100])    
+    @pytest.mark.parametrize("h2", [-0.2, -0.01, 1.01, 1.2, "a", "1", [0.4,0.3]])
+    def test_h2error(self, size, h2):
+        G = np.random.normal(size = size)            
+        with pytest.raises(ValueError, match="Heritability should be 0 <= h2 <= 1"):
+            phenotype, E = sim_pheno.environment(G, h2, 1)    
 
 class Test_choose_causal:
-    def test_choose_causal(self):
-        rng = np.random.default_rng(np.random.randint(10000))
-        num_mutations_list = np.array([1000, 2000, 5000, 10000])
-        num_causal_list = np.array([1, 10, 20, 100, 1000])
-        trait_sd_list = np.array([0.1, 0.3, 0.5, 0.7, 0.9])
-        for num_mutations in num_mutations_list:
-            for num_causal in num_causal_list:
-                for trait_sd in trait_sd_list:
-                    mutation_id, beta = sim_pheno.choose_causal(num_mutations, num_causal, trait_sd, rng)
-                    assert len(beta) == num_causal
-                    assert len(mutation_id) == num_causal
-                    assert min(mutation_id) >= 0
-                    assert max(mutation_id) < num_mutations
-    def test_error_num_sites(self):
-        rng = np.random.default_rng(np.random.randint(10000))
-        num_mutations_list = np.array([100, 300, 500, 700])
-        addition_list = np.array([1, 10, 100, 1000])
-        trait_sd_list = np.array([0.1, 0.3, 0.5, 0.7, 0.9])
-        for num_mutations in num_mutations_list:
-            for addition in addition_list:
-                for trait_sd in trait_sd_list:
-                    with pytest.raises(ValueError):
-                        num_causal = num_mutations + addition
-                        mutation_id, beta = sim_pheno.choose_causal(num_mutations, num_causal, trait_sd, rng)
-    def test_error_trait_sd(self):
-        rng = np.random.default_rng(np.random.randint(10000))
-        num_mutations_list = np.array([1000, 2000, 5000, 10000])
-        num_causal_list = np.array([1, 10, 20, 100, 1000])
-        trait_sd_list = np.array([0, -0.1, -1, -10])
-        for num_mutations in num_mutations_list:
-            for num_causal in num_causal_list:
-                for trait_sd in trait_sd_list:
-                    with pytest.raises(ValueError):
-                        mutation_id, beta = sim_pheno.choose_causal(num_mutations, num_causal, trait_sd, rng)
+    @pytest.mark.parametrize("num_mutations", [10, 100, 1000])
+    @pytest.mark.parametrize("num_causal", [1, 2, 10])
+    @pytest.mark.parametrize("trait_sd", [0.1, 0.3, 0.5, 0.7, 0.9])
+    def test_choose_causal(self, num_mutations, num_causal, trait_sd):
+        rng = np.random.default_rng(1)
+        mutation_id, beta = sim_pheno.choose_causal(num_mutations, num_causal, trait_sd, rng)
+        assert len(beta) == num_causal
+        assert len(mutation_id) == num_causal
+        assert min(mutation_id) >= 0
+        assert max(mutation_id) < num_mutations
+        assert np.issubdtype(beta.dtype, np.floating)
+        assert np.issubdtype(mutation_id.dtype, np.integer)
 
-            
+    @pytest.mark.parametrize("num_mutations", [10, 100, 1000])
+    @pytest.mark.parametrize("trait_sd", [0.1, 0.3, 0.5, 0.7, 0.9])    
+    def test_zero(self, num_mutations, trait_sd):
+        rng = np.random.default_rng(1)
+        num_causal = 0
+        mutation_id, beta = sim_pheno.choose_causal(num_mutations, num_causal, trait_sd, rng)
+        assert len(beta) == 0
+        assert len(mutation_id) == 0
+    
+    @pytest.mark.parametrize("addition", [1, 2, 10, 100])
+    @pytest.mark.parametrize("num_mutations", [1, 2, 10, 100])
+    @pytest.mark.parametrize("trait_sd", [0.1, 0.3, 0.5, 0.7, 0.9]) 
+    def test_error_num_sites(self, addition, num_mutations, trait_sd):
+        rng = np.random.default_rng(2)
+        with pytest.raises(ValueError, match="There are more causal sites than the number of mutations inside the tree sequence"):
+            num_causal = num_mutations + addition
+            mutation_id, beta = sim_pheno.choose_causal(num_mutations, num_causal, trait_sd, rng)
+    
+    @pytest.mark.parametrize("num_mutations", [10, 100, 1000])
+    @pytest.mark.parametrize("trait_sd", [0.1, 0.3, 0.5, 0.7, 0.9]) 
+    @pytest.mark.parametrize("num_causal", [-1, 1.0, "1", "a"]) 
+    def test_error_num_causal(self, num_mutations, trait_sd, num_causal):
+        rng = np.random.default_rng(3)
+        with pytest.raises(ValueError, match = "Number of causal sites should be a non-negative integer"):
+            mutation_id, beta = sim_pheno.choose_causal(num_mutations, num_causal, trait_sd, rng)
+        
+    @pytest.mark.parametrize("num_mutations", [10, 100, 1000])
+    @pytest.mark.parametrize("trait_sd", [0, -0.1, -1, -10, "1", [1,1]])
+    @pytest.mark.parametrize("num_causal", [0, 1, 2, 10])    
+    def test_error_trait_sd(self, num_mutations, trait_sd, num_causal):
+        rng = np.random.default_rng(np.random.randint(5))
+        with pytest.raises(ValueError, match = "Standard deviation should be a non-negative number"):
+            mutation_id, beta = sim_pheno.choose_causal(num_mutations, num_causal, trait_sd, rng)
 
+         
 class Test_parse_genotypes:
-    def test_parse_genotypes(self):
-        seeds = np.random.randint(1, 2**16, 3)
-        num_causal_list = np.array([1, 10, 50, 100])
-        size = 1000
-        for seed in seeds:
-            rng = np.random.default_rng(seed)
-            ts = msprime.sim_ancestry(size, sequence_length=100_000, recombination_rate=1e-8,population_size=10**4, random_seed=seed)
-            ts = msprime.sim_mutations(ts, rate=1e-8, random_seed=seed, model="binary", discrete_genome=False)
-            for num_causal in num_causal_list:
-                mutation_id = rng.choice(range(ts.num_mutations), size=num_causal, replace=False)
-                beta = rng.normal(size=num_causal)
-                G, location, mutation_list = sim_pheno.parse_genotypes(ts, mutation_id, beta)
-                assert len(G) == ts.num_individuals
-                assert len(location) == num_causal
-                assert len(mutation_list) == num_causal
-                
+    @pytest.mark.parametrize("seed", [1, 3, 5, 7, 9])
+    @pytest.mark.parametrize("num_causal", [1, 2, 10])
+    def test_parse_genotypes(self, seed, num_causal):
+        rng = np.random.default_rng(seed)
+        ts = msprime.sim_ancestry(100, sequence_length=10_000, recombination_rate=1e-8,population_size=10**4, random_seed=seed)
+        ts = msprime.sim_mutations(ts, rate=1e-8, random_seed=seed, model="binary", discrete_genome=False)
+        mutation_id, beta = sim_pheno.choose_causal(ts.num_mutations, num_causal, 1, rng)
+        G, location, mutation_list = sim_pheno.parse_genotypes(ts, mutation_id, beta)
+        assert len(G) == ts.num_individuals
+        assert len(location) == num_causal
+        assert len(mutation_list) == num_causal
+        assert np.issubdtype(G.dtype, np.floating)
+        assert np.issubdtype(location.dtype, np.integer) or np.issubdtype(location.dtype, np.floating)
+        assert np.issubdtype(mutation_list.dtype, np.integer)
+    
+    def test_tree_sequence_binary(self):
+        ts = tskit.Tree.generate_comb(6, span=20).tree_sequence
+        tables = ts.dump_tables()
+        for j in range(10):
+            tables.sites.add_row(j, "A")
+            tables.mutations.add_row(site=j, node=j, derived_state="T")
+        ts = tables.tree_sequence()
+        mutation_id = np.array(list(range(10)))
+        beta = np.array(list(range(10)))
+        G, location, mutation_list = sim_pheno.parse_genotypes(ts, mutation_id, beta)
+        
+        G_actual = np.array([10, 46, 69])
+        
+        assert np.array_equal(mutation_list, mutation_id)
+        assert np.array_equal(G, G_actual)
+        
+    def test_tree_sequence_nonbinary(self):
+        ts = tskit.Tree.generate_balanced(6, arity=4, span=10).tree_sequence
+        tables = ts.dump_tables()
+        for j in range(7):
+            tables.sites.add_row(j, "A")
+            tables.mutations.add_row(site=j, node=j, derived_state="T")
+        ts = tables.tree_sequence()
+        mutation_id = np.array([0, 1, 3, 6])
+        beta = np.repeat(1,4)
+        G, location, mutation_list = sim_pheno.parse_genotypes(ts, mutation_id, beta)
+        G_actual = np.array([2,2,2])
+        
+        assert np.array_equal(mutation_list, mutation_id)
+        assert np.array_equal(G, G_actual)
+
+               
 class Test_phenotype_sim:
-    def test_phenotype_sim(self):
-        seeds = np.random.randint(1, 2**16, 3)
-        num_causal_list = np.array([1, 10, 50, 100])
-        size = 1000
-        for seed in seeds:
-            rng = np.random.default_rng(seed)
-            ts = msprime.sim_ancestry(size, sequence_length=100_000, recombination_rate=1e-8,population_size=10**4, random_seed=seed)
-            ts = msprime.sim_mutations(ts, rate=1e-8, random_seed=seed, model="binary", discrete_genome=False)
-            for num_causal in num_causal_list:
-                pheno_df, gene_df = sim_pheno.phenotype_sim(ts, num_causal, trait_sd=1, h2=0.3, seed=seed)
-                
-                # Check if any values of pheno_df / gene_df is null
-                assert pheno_df.isnull().values.any() == False
-                assert gene_df.isnull().values.any() == False
-                
-                # Check dimension
-                assert len(pheno_df) == ts.num_individuals
-                assert len(gene_df) == num_causal
+    @pytest.mark.parametrize("seed", [1, 2, 3, 4])
+    @pytest.mark.parametrize("num_causal", [1, 2, 10])
+    def test_phenotype_sim(self, seed, num_causal):
+        size = 20
+        ts = msprime.sim_ancestry(size, sequence_length=100_000, recombination_rate=1e-8,population_size=10**4, random_seed=seed)
+        ts = msprime.sim_mutations(ts, rate=1e-8, random_seed=seed, model="binary", discrete_genome=False)
+        pheno_df, gene_df = sim_pheno.phenotype_sim(ts, num_causal, trait_sd=1, h2=0.3, seed=seed)
+        assert pheno_df.isnull().values.any() == False
+        assert gene_df.isnull().values.any() == False
+        assert len(pheno_df) == ts.num_individuals
+        assert len(gene_df) == num_causal
+    
+    @pytest.mark.parametrize("seed", [1, 2, 3, 4])
+    @pytest.mark.parametrize("num_causal", [1, 2, 10])
+    def test_no_mutation(self, seed, num_causal):
+        size = 20
+        rng = np.random.default_rng(seed)
+        ts = msprime.sim_ancestry(size, sequence_length=100_000, recombination_rate=1e-8,population_size=10**4, random_seed=seed)
+        with pytest.raises(ValueError, match = "No mutation in the provided data"):
+            pheno_df, gene_df = sim_pheno.phenotype_sim(ts, num_causal, trait_sd=1, h2=0.3, seed=seed)
--- treeGWAS/sim_phenotypes.py
+++ treeGWAS/sim_phenotypes.py
@@ -5,20 +5,38 @@ import pandas as pd
 
 
 def choose_causal(num_mutations, num_causal, trait_sd, rng):
+    if not isinstance(num_causal, int) or num_causal < 0:
+        raise ValueError("Number of causal sites should be a non-negative integer")
+    if num_mutations == 0:
+        raise ValueError("No mutation in the provided data")
+    if not isinstance(num_mutations, int) or num_mutations < 0:
+        raise ValueError("Number of mutation sites should be a non-negative integer")
     if num_causal > num_mutations:
-        raise ValueError("Too many causal sites")
-    if trait_sd <= 0:
-        raise ValueError("Standard deviation should be greater than 0")
+        raise ValueError("There are more causal sites than the number of mutations inside the tree sequence")
+    if (not isinstance(trait_sd, int) and not isinstance(trait_sd, float)) or trait_sd <= 0:
+        raise ValueError("Standard deviation should be a non-negative number")
     mutation_id = rng.choice(range(num_mutations), size=num_causal, replace=False)
-    beta = rng.normal(loc=0, scale=trait_sd/ np.sqrt(num_causal), size=num_causal)
+    if num_causal > 0:
+        beta = rng.normal(loc=0, scale=trait_sd/ np.sqrt(num_causal), size=num_causal)
+    else:
+        beta = np.array([])
     return mutation_id, beta
 
-def environment(G, h2):
-    if h2 >= 1 or h2 <= 0:
-        raise ValueError("Heritability should be 0 < h2 < 1")
+def environment(G, h2, trait_sd):
+    if len(G) == 0:
+        raise ValueError("No individuals in the simulation model")
+    if (not isinstance(h2, int) and not isinstance(h2, float)) or h2 > 1 or h2 < 0:
+        raise ValueError("Heritability should be 0 <= h2 <= 1")
     num_ind = len(G)
-    E = np.random.normal(loc=0.0, scale=np.sqrt((1-h2)/h2 * np.var(G)), size=num_ind)
-    phenotype = G + E
+    if h2 == 1:
+        E = np.zeros(num_ind)
+        phenotype = G
+    elif h2 == 0:
+        E = np.random.normal(loc=0.0, scale=trait_sd, size=num_ind)
+        phenotype = E
+    else:
+        E = np.random.normal(loc=0.0, scale=np.sqrt((1-h2)/h2 * np.var(G)), size=num_ind)
+        phenotype = G + E
     return phenotype, E
 
 def update_node_values_tree_object(tree, node_values, G):
@@ -45,19 +63,22 @@ def update_node_values_array_access(root, left_child_array, right_sib_array, nod
             node_values[child] += node_values[parent]
             stack.append(child)
             right_sib = right_sib_array[child]
-            assert right_sib != -1
-            node_values[right_sib] += node_values[parent]
-            stack.append(right_sib)
+            while right_sib != -1:
+                node_values[right_sib] += node_values[parent]
+                stack.append(right_sib)
+                right_sib = right_sib_array[right_sib]
         else:
             G[parent] += node_values[parent]
     return G
 
 def parse_genotypes(ts, mutation_id, beta):
+    if type(ts) != tskit.trees.TreeSequence:
+        raise TypeError("Input should be a tree sequence data")  
     size_G = np.max(ts.samples())+1
     size_mutation = np.max(mutation_id)+1
     G = np.zeros(size_G)
     location = np.zeros(len(mutation_id))
-    mutation_list = np.zeros(len(mutation_id))
+    mutation_list = np.zeros(len(mutation_id), dtype=int)
     snp_idx = 0
     for tree in ts.trees():
         node_values = np.zeros(ts.num_nodes)
@@ -77,11 +98,13 @@ def parse_genotypes(ts, mutation_id, beta):
     return G, location, mutation_list
 
 def phenotype_sim(ts, num_causal, trait_sd=1, h2=0.3, seed=1):
+    if type(ts) != tskit.trees.TreeSequence:
+        raise TypeError("Input should be a tree sequence data")    
     rng = np.random.default_rng(seed)
     mutation_id, beta = choose_causal(ts.num_mutations, num_causal, trait_sd, rng)
     # This G is genotype of individuals
     G, location, mutation_list = parse_genotypes(ts, mutation_id, beta)
-    phenotype, E = environment(G, h2)
+    phenotype, E = environment(G, h2, trait_sd)
     assert len(phenotype) == ts.num_individuals
     
     
