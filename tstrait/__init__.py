456e819aefeee0a71d7688fdda36f3386ae92a56
daikitag <48062118+daikitag@users.noreply.github.com>
2023-06-10 14:44:34 +0900

    Update __init__.py

---
+++ tstrait/__init__.py
@@ -0,0 +1,22 @@
+from tstrait.simulate_phenotype import (
+    sim_phenotype,
+    PhenotypeSimulator,
+    GeneticValueResult,
+    PhenotypeResult
+)
+
+from tstrait.trait_model import (
+    TraitModel,
+    TraitModelAdditive,
+    TraitModelAllele
+)
+
+__all__ = [
+    "sim_phenotype",
+    "PhenotypeSimulator",
+    "GeneticValueResult",
+    "PhenotypeResult",
+    "TraitModel",
+    "TraitModelAdditive",
+    "TraitModelAllele"
+]
