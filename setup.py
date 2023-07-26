22cbf3187cb917ddb670a619b939cf9167fe3099
daikitag <48062118+daikitag@users.noreply.github.com>
2023-07-26 21:03:51 +0900

    setup

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 import setuptools
+import os
+
+tstrait_version = None
+version_file = os.path.join("tstrait", "_version.py")
+with open(version_file) as f:
+    exec(f.read())
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup(name="tstrait", version=tstrait_version)
