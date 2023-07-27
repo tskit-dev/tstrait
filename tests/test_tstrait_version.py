1a6f1dd4649ff22c15e7bb38a416f347acadd602
daikitag <48062118+daikitag@users.noreply.github.com>
2023-07-27 10:27:41 +0900

    version test

---
+++ tests/test_tstrait_version.py
@@ -0,0 +1,13 @@
+from packaging.version import Version
+
+import tstrait
+
+
+class TestPythonVersion:
+    """
+    Test that the version is PEP440 compliant
+    """
+
+    def test_version(self):
+        assert str(Version(tstrait._version.tstrait_version)) == tstrait._version.tstrait_version
+        assert tstrait.__version__ == tstrait._version.tstrait_version
